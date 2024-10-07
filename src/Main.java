be6f6d76ed11c82d3c375dcccb2645d8d289560e
CevicheConAji <piero24112000@gmail.com>
2024-10-07 02:24:44 +0200

    moverFichero

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner sc = new Scanner(System.in);
-    public static String rutafichero = "";
+    public static String rutafichero = "/Users/piero/javatest";
 
     public static void main(String[] args) {
 
-        ///Users/piero/javatest
+        //Users/piero/javatest
 
         test();
+
 
     }
     public static void test() {
@@ -87,14 +92,37 @@ public class Main {
         System.out.print("***Inserte la ruta del archivo que desea cambiar el nombre*** " +
                 "\nUsted esta en :"+rutafichero);
         ficheroCambiar = sc.nextLine();
-
         File fichero = new File(rutafichero+ficheroCambiar);
         if(fichero.exists()){
             System.out.print("***Introduzca la ruta y el nuevo nombre del archivo***. " +
                     "\nUsted esta en :"+rutafichero);
             ficheroNuevo = sc.nextLine();
+            //Arreglar
             fichero.renameTo(new File(rutafichero+ficheroNuevo));
             System.out.println("Se cambio el nombre del fichero "+ficheroNuevo+" a "+ficheroNuevo);
+            moverFicheroConFiles();
+        }
+    }
+    public static void moverFicheroConFiles(){
+        String ficheroMover;
+        String directorioNuevo = "";
+        String nuevoNombre ;
+
+        System.out.println("MOVER FICHERO");
+        System.out.print("Introduzca el fichero que quiere mover\nUsted esta en :"+rutafichero);
+        ficheroMover = sc.nextLine();
+        System.out.print("Introduzca la ruta a donde quieres mover el fichero \nUsted esta en :"+rutafichero);
+        directorioNuevo = sc.nextLine();
+        String[] s = ficheroMover.split("/");
+
+        nuevoNombre = s[s.length-1];
+        System.out.println("Se movio el fichero:"+nuevoNombre+" a "+directorioNuevo);
+
+        try {
+            Files.move(Paths.get(rutafichero+ficheroMover),
+                    Paths.get(rutafichero+directorioNuevo).resolve(nuevoNombre),StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
